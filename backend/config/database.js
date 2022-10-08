56b758f0c5994eb184b4f9ba6d3d60099d75e7c8
Siddharth <officialsiddharthbisht@gmail.com>
2022-10-08 13:56:17 +0530

    ADD: User schema design done

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 exports.connectDatabase = () =>{
     mongoose
-        .connect(process.env.MONGO_URI)
+        .connect(process.env.MONGO_URI,{
+            useNewUrlParser:true ,
+            useUnifiedTopology : true
+        })
         .then((conn) => console.log(`Database Connected : ${conn.connection.host}`))
         .catch((err)=>console.log(err))
 }
